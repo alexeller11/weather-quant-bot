9bc0c80996aedd84ebdbdbad7d0a452715bb8ff8
alexeller11 <88058746+alexeller11@users.noreply.github.com>
2026-07-14 15:06:51 -0300

    Delete analytics/engine.py

--- analytics/engine.py
+++
@@ -1,7 +0,0 @@
-from analytics.dataset import build_dataset
-from analytics.registry import registry
-import analytics.register_metrics
-
-dataset = build_dataset(bankroll)
-
-analytics = registry.execute(dataset)
